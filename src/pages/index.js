ace8bd5ef233c3b223e9d5b6617bc07ecd4041f5
Ray Sanders <raymond.sanders@gmail.com>
2021-01-05 22:26:02 -0600

    Add rough avatar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import linkContent from '../../content/linkContent.yaml'
+import avatar from '../../content/images/WeCominForYouCastCover.jpg'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div style={{ maxWidth: `680px`, marginBottom: `1.45rem`}}>  
-      <div>{linkContent.title}</div>    
+      <div>{linkContent.title}</div> 
+      <div><img src={avatar} alt="" /></div>
       <div>{linkContent.tagline}</div>
       <div style={{marginTop: '1.45rem'}}>
         {linkContent.links.map(link => 
