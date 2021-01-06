8a896361de62afe0346bcb5eda6c45092cae95ec
Ray Sanders <raymond.sanders@gmail.com>
2021-01-05 21:50:45 -0600

    Quick styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,10 @@ import linkContent from '../../content/linkContent.yaml'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div style={{ maxWidth: `680px`, marginBottom: `1.45rem` }}>  
+    <div style={{ maxWidth: `680px`, marginBottom: `1.45rem`}}>  
       <div>{linkContent.title}</div>    
       <div>{linkContent.tagline}</div>
-      <div>
+      <div style={{marginTop: '1.45rem'}}>
         {linkContent.links.map(link => 
           <div>
           <a href={link.url}>{link.name}</a>
