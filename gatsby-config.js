efafc83d9911176171b1f78000f63d54dda3a8fc
Ray Sanders <raymond.sanders@gmail.com>
2021-01-05 20:59:35 -0600

    Customize wcfy data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `We Comin' For You Cast Links`,
+    description: `Listen wherever you get your podcasts`,
+    author: `@RaySanders7`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
